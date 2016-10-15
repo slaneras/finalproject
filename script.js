8217b1a8dcab80ba16772aac7230b0cc257472b4
Sarena Ahmad <slaneras@gmail.com>
2016-10-15 12:55:55 +0800

    Changes

--- script.js
+++ script.js
@@ -10,12 +10,20 @@ $(document).ready(function() {
         .mouseleave(function() {
             timer = setInterval(function() {
                 $('.carousel > div:first')
-                    .fadeOut(3000)
-                    .next()
-                    .fadeIn(3000)
-                    .end()
-                    .appendTo('.carousel');
-            }, 6000);
+                    .fadeOut(3000, function() {
+                        $(this).next()
+                            .fadeIn(3000)
+                        $(this).appendTo('.carousel');
+                    })
+                    // .end()
+            }, 3000);
+            // timer = setInterval(function() {
+            //     var visibleDiv = $('.carousel > div:visible')
+            //     var nextDiv = visibleDiv.next().length ? visibleDiv.next() : $('.carousel > div:first')
+            //     visibleDiv.fadeOut(3000, function () {
+            //       nextDiv.fadeIn(3000)
+            //     })
+            // }, 4000);
         })
         .mouseleave();
 });
